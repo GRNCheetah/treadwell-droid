33c654492efb7f76a5da31f1d244f40330f4af86
GRNCheetah <greencheetah10@yahoo.com>
2018-01-05 16:44:04 -0600

    Added bot control to the main

--- BotControlpart2.py
+++ BotControlpart2.py
@@ -5,14 +5,16 @@ class ArduinoControl:
 
     def __init__(self):
     
+        # PWM outs on the Arduino
         self.lF = 10
         self.rF = 5
         self.lR = 11
         self.rR = 3
-
+        
+        # Should be between 0-255, speed of the motors
         self.speed = 0
         
-    def connect2Arduino(self):
+        # Sets connections to arduino
         try:
             self.connection = SerialManager()
             aelf.a = ArduinoApi(connection = self.connection)
@@ -22,30 +24,33 @@ class ArduinoControl:
             self.a.pinMode(self.rR, self.a.OUTPUT)
             print("[+] Connected")
         except:
-            print("[x] Failed to connect to Arduino")
-        
+            print("[x] Failed to connect to Arduino")    
+    
     def makeAMove(self):
-        
+
+        # Will try to move at the given speed
         try:
-            a.analogWrite(self.lF, self.speed)
-            a.analogWrite(self.rF, self.speed)
-            a.analogWrite(self.lR, self.speed)
-            a.analogWrite(self.rR, self.speed)
+            self.a.analogWrite(self.lF, self.speed)
+            self.a.analogWrite(self.rF, self.speed)
+            self.a.analogWrite(self.lR, self.speed)
+            self.a.analogWrite(self.rR, self.speed)
         except:
             print("[x] System Error")
             print("[x] stopping...")
-            a.analogWrite(self.lF, 0)
-            a.analogWrite(self.rF, 0)
-            a.analogWrite(self.lR, 0)
-            a.analogWrite(self.rR, 0)
+            self.a.analogWrite(self.lF, self.a.LOW)
+            self.a.analogWrite(self.rF, self.a.LOW)
+            self.a.analogWrite(self.lR, self.a.LOW)
+            self.a.analogWrite(self.rR, self.a.LOW)
     
-    def incSpeed(self, speed):
+    def incSpeed(self):
         self.speed += 10
-            
         
+    def decSpeed(self):
+        self.speed -= 10
+        
+    def stop(self):
+        self.a.analogWrite(self.lF, self.a.LOW)
+        self.a.analogWrite(self.rF, self.a.LOW)
+        self.a.analogWrite(self.lR, self.a.LOW)
+        self.a.analogWrite(self.rR, self.a.LOW)
             
-            
-            
-            
-meme = ArduinoControl()
-meme.connect2Arduino()
--- main.py
+++ main.py
@@ -1,30 +1,61 @@
 import Eyes
+import ArduinoControl
 
 import pygame
-
-pygame.init()
-pygame.mixer.pre_init()
+from pygame.locals import *
+import sys
+from nanpy import (ArduinoApi, SerialManager)
+import time
 
 ''' COLORS '''
 colGreen = pygame.Color(0,255,0)
 colBlack = pygame.Color(0,0,0)
 
 ''' GAME DISPLAY '''
+pygame.init()
+pygame.mixer.pre_init()
+
 # (width, height)
 dispWH = (1600,900)
 
 gameDisplay = pygame.display.set_mode(dispWH,pygame.FULLSCREEN, 32)
 pygame.display.set_caption("Treadwell")
 
+# Start both utilities
 screen = Eyes.Eyes(gameDisplay)
+bot = ArduinoControl.ArduinoControl()
 
-
-while True:
-    # Reset for the screen, I want this in the Eyes.py file
-    gameDisplay.fill(colBlack)
-
-    screen.blink(colGreen)
-
+def main()
+    while True:
     
-    pygame.display.update()
+        # Moves the bot at the given speed
+        bot.makeAMove()
+        
+        
+        # Just for effect
+        screen.idle(colGreen)
+    
+        for event in pygame.event.get():
+        
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+                
+            elif event.type == KEYDOWN:
+                if event.key == K_q:
+                    pygame.quit()
+                    sys.exit()
+                    
+                elif event.key == K_UP:
+                    bot.incSpeed()
+                elif event.key == K_DOWN:
+                    bot.decSpeed()
+                elif event.key == K_SPACE:
+                    bot.stop()
+        
+        
+        
+        
+        # Last thing to be called
+        pygame.display.update()
     
